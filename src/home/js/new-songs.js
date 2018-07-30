7127d353d82e6824819b71f7e152205d8dce7ddc
HappyJeannie <436030684@qq.com>
2018-07-30 10:12:54 +0800

    【增加】- 首页 - 最新音乐列表

---
+++ src/home/js/new-songs.js
@@ -0,0 +1,70 @@
+{
+  let view = {
+    el: '#newSongList',
+    liTpl: `
+      <li>
+        <div class="msg">
+          <p>__name__</p>
+          <p>
+            <i class="hot"></i>__singer__</p>
+        </div>
+        <div class="play">
+          <a href="__url__"><i class="icon iconfont icon-play1"></i></a>
+        </div>
+      </li>
+    `,
+    ulTpl : `<ul>__list__</ul>`,
+    render(data){
+      let $html = '';
+      console.log(data);
+      for(let i = 0;i<data.length;i++){
+        let $li = this.liTpl;
+        for(var key in data[i]){
+          $li = $li.replace(`__${key}__`,data[i][key]);
+        }
+        console.log($li)
+        $html += $li;
+      }
+      let html = this.ulTpl.replace('__list__',$html);
+      $(this.el).html(html);
+    }
+  }
+  let model = {
+    data : null,
+    fetchAll : function(){
+      // 获取所有歌曲
+      let query = new AV.Query('Songs');
+      return query.find().then(
+        (res) => {
+          let data = [];
+          for(let i = 0 ;i<res.length;i++){
+            let song = {
+              id : res[i].id,
+              ...res[i].attributes
+            }
+            data.push(song);
+          }
+          return data;
+        }
+      )
+    }
+  }
+  let controller = {
+    init(view, model) {
+      this.view = view;
+      this.model = model;
+      this.model.fetchAll()
+        .then(
+          (res) => {
+            console.log('请求成功')
+            console.log(res);
+            this.model.data = res;
+            console.log(this);
+            this.view.render(this.model.data);
+            console.log(2);
+          }
+      )
+    }
+  }
+  controller.init(view, model);
+}
