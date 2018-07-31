d29a3dda7fc136f1e062179dc16a14a55c235a68
HappyJeannie <436030684@qq.com>
2018-07-31 10:38:13 +0800

    【增加】- 详情 - 歌词滚动

--- src/detail/js/detail.js
+++ src/detail/js/detail.js
@@ -7,6 +7,13 @@
     render(data){
       $(this.el).append(this.tpl.replace('__url__',data.url));
       //$(this.el).find('audio').get(0).playbackRate = 3.0;
+      let arr = data.lyrics.split('\n');
+      for(let i = 0;i<arr.length;i++){
+        let reg = /\[([\d:.]+)\](.+)/;
+        let a = arr[i].match(reg);
+        let time = parseInt(a[1].substr(0,2)) * 60 * 1000 + parseInt(a[1].substr(3,2)) * 1000 + parseInt(a[1].substr(6,2)) + 4000;
+        $(this.el).find('.lyrics').append(`<p class='${i===0? "active" : ""}' data-time='${time}'>${a[2]}</p>`)
+      }
     },
     play(){
       let audio = $(this.el).find('audio').get(0);
@@ -56,6 +63,8 @@
             this.songEnded();
             // 添加cover
             this.addCover();
+            // 监听音乐播放的过程中的时间
+            this.songPlaying();
           }
         )
     },
@@ -90,6 +99,28 @@
       $(this.view.el).find('.song').css({
         'background-image':`url(${this.model.data.cover})`
       })
+    },
+    songPlaying(){
+      $(this.view.el).find('audio').get(0).ontimeupdate = (e)=>{
+        let currentTime = parseInt(e.timeStamp);
+        console.log(currentTime / 1000)
+        let allP = $(this.view.el).find('.lyrics>p');
+        let idx = 0;
+        let minTime = Math.abs(currentTime - parseInt($(allP[0]).attr('data-time')));
+        for(let i = 1;i<allP.length;i++){
+          if(Math.abs(parseInt($(allP[i]).attr('data-time')) - currentTime) < minTime){
+            minTime = Math.abs(parseInt($(allP[i]).attr('data-time')) - currentTime);
+            idx = i;
+          }
+        }
+        $(allP[idx]).addClass('active').siblings().removeClass('active');
+        let pTop = $(allP[idx]).offset().top;
+        let lyricsTop = $(this.view.el).find('.lyrics').offset().top;
+        let target = $(this.view.el).find('.lyrics');
+        let scroll = parseInt(target.attr('data-top'))+pTop-lyricsTop - 26;
+        target.attr('data-top',scroll).scrollTop(scroll)
+        //target.attr('data-top',scroll).animate({scrollTop: scroll+'px'}, 300);
+      }
     }
   }
   controller.init(view,model);
