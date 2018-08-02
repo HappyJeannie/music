16961682adf535394b9494fc6b3110cb45e2a5be
HappyJeannie <436030684@qq.com>
2018-08-02 15:42:48 +0800

    【增加】- 配置文件

---
+++ src/vendors/initializers/av.js
@@ -0,0 +1,10 @@
+{
+  let APP_ID = 'YPvb9cfXnyvVeG54DHsOIscm-gzGzoHsz';        
+  let APP_KEY = '4TgQlSLvQtMXkzQcxoC3sLQE';
+
+  
+  AV.init({                             
+    appId: APP_ID,
+    appKey: APP_KEY
+  });
+}
